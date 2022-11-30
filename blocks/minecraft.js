6dfd86bdf2c7c3dc72187f3fe1a6de79de246a8b
Алексей <mablexa@ya.ru>
2022-11-30 17:37:09 +0300

    Update minecraft.js

--- blocks/minecraft.js
+++ blocks/minecraft.js
@@ -296,8 +296,8 @@ Blockly.Blocks['minecraft_weather'] = {
         this.appendDummyInput()
             .appendField("Погода на сервере")
             .appendField(new Blockly.FieldDropdown([["солнечная", "clear"], ["дождь", "rain"], ["буря", "thunder"]]), "Type")
-			.appendField("мин. продолж.")
-			.appendField(new Blockly.FieldNumber(0), "duration");
+	    .appendField("мин. продолж.")
+	    .appendField(new Blockly.FieldNumber(0), "duration");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -310,11 +310,11 @@ Blockly.Blocks['minecraft_weather'] = {
 
 Blockly.Blocks['minecraft_text'] = {
     init: function () {
-		this.appendDummyInput().appendField("Текст в чат");
-		this.appendDummyInput()
+	this.appendDummyInput().appendField("Текст в чат");
+	this.appendDummyInput()
             .appendField("цвет")
             .appendField(new Blockly.FieldDropdown([["зелёный", "green"], ["жёлтый", "yellow"], ["красный", "red"]]), "color");
-		this.appendValueInput("mctext") .setCheck("String");
+	this.appendValueInput("mctext") .setCheck("String");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -383,8 +383,8 @@ Blockly.Blocks['minecraft_craft'] = {
         this.appendDummyInput()
             .appendField("Скрафтить предмет: ")
             .appendField(new Blockly.FieldDropdown([["Палка", "stick"], ["Доски", "oak_planks"], ["Деревянный меч", "wooden_sword"]]), "item")
-			      .appendField("Кол-во: ")
-			      .appendField(new Blockly.FieldNumber(1), "count");
+	    .appendField("Кол-во: ")
+	    .appendField(new Blockly.FieldNumber(1), "count");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -476,13 +476,13 @@ Blockly.Blocks['minecraft_nearright'] = {
 
 Blockly.Blocks['minecraft_playnote'] = {
     init: function () {
-		    this.appendDummyInput().appendField("Проигрывать. Октава");
-		    this.appendDummyInput()
-			      .appendField(new Blockly.FieldDropdown([["0", "0"], ["1", "1"]]), "octave")
-			      .appendField("тон")
+	this.appendDummyInput().appendField("Проигрывать. Октава");
+	this.appendDummyInput()
+	    .appendField(new Blockly.FieldDropdown([["0", "0"], ["1", "1"]]), "octave")
+	    .appendField("тон")
             .appendField(new Blockly.FieldDropdown([["A", "A"], ["B", "B"], ["C", "C"], ["D", "D"], ["E", "E"], ["F", "F"], ["G", "G"]]), "tone")
-			      .appendField("инструмент")
-			      .appendField(new Blockly.FieldDropdown([["Банджо", "BANJO"], ["Большой барабан", "BASS_DRUM"], ["Бас-гитара", "BASS_GUITAR"], ["Колокол", "BELL"], ["Бит", "BIT"], ["Звон", "CHIME"], ["Колокольчик", "COW_BELL"], ["Диджериду", "DIDGERIDOO"], ["Флейта", "FLUTE"], ["Гитара", "GUITAR"], ["Железный ксилофон", "IRON_XYLOPHONE"], ["Пианино", "PIANO"], ["Плинг", "PLING"], ["Малый барабан", "SNARE_DRUM"], ["Палочки", "STICKS"], ["Ксилофон", "XYLOPHONE"]]), "instrument");
+	    .appendField("инструмент")
+	    .appendField(new Blockly.FieldDropdown([["Банджо", "BANJO"], ["Большой барабан", "BASS_DRUM"], ["Бас-гитара", "BASS_GUITAR"], ["Колокол", "BELL"], ["Бит", "BIT"], ["Звон", "CHIME"], ["Колокольчик", "COW_BELL"], ["Диджериду", "DIDGERIDOO"], ["Флейта", "FLUTE"], ["Гитара", "GUITAR"], ["Железный ксилофон", "IRON_XYLOPHONE"], ["Пианино", "PIANO"], ["Плинг", "PLING"], ["Малый барабан", "SNARE_DRUM"], ["Палочки", "STICKS"], ["Ксилофон", "XYLOPHONE"]]), "instrument");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -494,13 +494,13 @@ Blockly.Blocks['minecraft_playnote'] = {
 
 Blockly.Blocks['minecraft_if'] = {
     init: function () {
-		    this.appendDummyInput().appendField("Если");
-		    this.appendDummyInput().appendField(new Blockly.FieldDropdown([["Блок в дроне", "blockin"], ["Блок слева", "blockleft"], ["Блок справа", "blockright"], ["Блок сзади", "blockback"]]), "whatif");
-		    this.appendDummyInput().appendField("==")
-			      .appendField(new Blockly.FieldVariable('mc_val_if'), "valif");
-		    this.appendDummyInput().appendField("то");
-		    this.appendDummyInput().appendField(new Blockly.FieldDropdown([["вперёд", "front"], ["назад", "back"], ["лево", "left"], ["право", "right"], ["Вверх", "up"], ["Вниз", "down"]]), "whatthen");
-		    this.appendDummyInput().appendField(new Blockly.FieldNumber(1), "valthen");
+	this.appendDummyInput().appendField("Если");
+	this.appendDummyInput().appendField(new Blockly.FieldDropdown([["Блок в дроне", "blockin"], ["Блок слева", "blockleft"], ["Блок справа", "blockright"], ["Блок сзади", "blockback"]]), "whatif");
+	this.appendDummyInput().appendField("==")
+	    .appendField(new Blockly.FieldVariable('mc_val_if'), "valif");
+	this.appendDummyInput().appendField("то");
+	this.appendDummyInput().appendField(new Blockly.FieldDropdown([["вперёд", "front"], ["назад", "back"], ["лево", "left"], ["право", "right"], ["Вверх", "up"], ["Вниз", "down"]]), "whatthen");
+	this.appendDummyInput().appendField(new Blockly.FieldNumber(1), "valthen");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -512,15 +512,15 @@ Blockly.Blocks['minecraft_if'] = {
 
 Blockly.Blocks['minecraft_movehand'] = {
     init: function () {
-		    this.appendDummyInput().appendField("Двигать ");
-		    this.appendDummyInput().appendField(new Blockly.FieldDropdown([["руками", "hand"], ["ногами", "leg"]]), "type");
-		    this.appendDummyInput().appendField(new Blockly.FieldDropdown([["правой", "right"], ["левой", "left"]]), "what");
-		    this.appendDummyInput().appendField("x");
-		    this.appendValueInput("x") .setCheck("Number");
-		    this.appendDummyInput().appendField("y");
-		    this.appendValueInput("y") .setCheck("Number");
-		    this.appendDummyInput().appendField("z");
-		    this.appendValueInput("z") .setCheck("Number");
+	this.appendDummyInput().appendField("Двигать ");
+	this.appendDummyInput().appendField(new Blockly.FieldDropdown([["руками", "hand"], ["ногами", "leg"]]), "type");
+	this.appendDummyInput().appendField(new Blockly.FieldDropdown([["правой", "right"], ["левой", "left"]]), "what");
+	this.appendDummyInput().appendField("x");
+	this.appendValueInput("x") .setCheck("Number");
+	this.appendDummyInput().appendField("y");
+	this.appendValueInput("y") .setCheck("Number");
+	this.appendDummyInput().appendField("z");
+	this.appendValueInput("z") .setCheck("Number");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -533,12 +533,12 @@ Blockly.Blocks['minecraft_movehand'] = {
 Blockly.Blocks['minecraft_getdroneblock'] = {
 	init: function() {
 		this.appendDummyInput()
-			  .appendField("(async) Запрос данных с сервера в")
-			  .appendField(new Blockly.FieldVariable('response'), "response");
+		    .appendField("(async) Запрос данных с сервера в")
+		    .appendField(new Blockly.FieldVariable('response'), "response");
 		this.appendStatementInput("otherCode")
-			  .setCheck(null)
-			  .appendField("от БОТ: ")
-			  .appendField(new Blockly.FieldCheckbox("FALSE"), "bot");
+	            .setCheck(null)
+		    .appendField("от БОТ: ")
+		    .appendField(new Blockly.FieldCheckbox("FALSE"), "bot");
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setColour(255);
@@ -551,9 +551,9 @@ Blockly.Blocks['minecraft_getServer'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Запрос данных")
-			      .appendField(new Blockly.FieldVariable('response'), "response")
-			      .appendField("от БОТ: ")
-			      .appendField(new Blockly.FieldCheckbox("FALSE"), "bot");
+	    .appendField(new Blockly.FieldVariable('response'), "response")
+	    .appendField("от БОТ: ")
+	    .appendField(new Blockly.FieldCheckbox("FALSE"), "bot");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -567,9 +567,9 @@ Blockly.Blocks['minecraft_setBlockData'] = {
 	init: function() {
 		this.appendDummyInput().appendField("Перенести данные блока");
 		this.appendDummyInput().appendField(new Blockly.FieldVariable('block_data'), "blockData");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+    		this.setInputsInline(true);
+    		this.setPreviousStatement(true, null);
+    		this.setNextStatement(true, null);
 		this.setColour("#cc0000");
 		this.setTooltip("Из данных блока полностью воссаздаёт блок на месте дрона");
 		this.setHelpUrl("");	
@@ -581,15 +581,15 @@ Blockly.Blocks['minecraft_jsonParse'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("JSON разбор");
-		    this.appendValueInput("server_string");
-		    this.appendDummyInput()
-			      .appendField(new Blockly.FieldDropdown([["Дрон/бот","drone"], ["Игрок","player"], ["Блоки рядом","botNearBlock"], ["Карта блоков","botMap"], ["Сущности рядом","botNearEntity"], ["Инвентарь (для бота)","inventory"]]), "type");
+	this.appendValueInput("server_string");
+	this.appendDummyInput()
+	    .appendField(new Blockly.FieldDropdown([["Дрон/бот","drone"], ["Игрок","player"], ["Блоки рядом","botNearBlock"], ["Карта блоков","botMap"], ["Сущности рядом","botNearEntity"], ["Инвентарь (для бота)","inventory"]]), "type");
         this.appendDummyInput()
             .appendField("Параметр:");
-		    this.appendValueInput("data");
-	    	this.appendDummyInput()
+	this.appendValueInput("data");
+	this.appendDummyInput()
             .appendField("Записать в")
-			      .appendField(new Blockly.FieldVariable('server_data'), "server_data");
+	    .appendField(new Blockly.FieldVariable('server_data'), "server_data");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -603,10 +603,10 @@ Blockly.Blocks['minecraft_coordBlock'] = {
     init: function() {
         this.appendDummyInput().appendField("Координаты X");
         this.appendValueInput("x").setCheck("Number");
-		    this.appendDummyInput().appendField("Y");
-		    this.appendValueInput("y").setCheck("Number");
-		    this.appendDummyInput().appendField("Z");
-		    this.appendValueInput("z").setCheck("Number");
+	this.appendDummyInput().appendField("Y");
+	this.appendValueInput("y").setCheck("Number");
+	this.appendDummyInput().appendField("Z");
+	this.appendValueInput("z").setCheck("Number");
         this.setInputsInline(true);
         this.setOutput(true, "String");
         this.setColour(255);
@@ -619,13 +619,13 @@ Blockly.Blocks['minecraft_droneSputnik'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Создать БОТ")
-			      .appendField("ближайший моб:")
-			      .appendField(new Blockly.FieldCheckbox("TRUE"), "zaxvat")
-			      .appendField("или")
-			      .appendField(new Blockly.FieldDropdown([["Человек","PLAYER"], ["Зомби","ZOMBIE"], ["Житель","VILLAGER"], ["Корова","COW"]]), "mob_type");
+	    .appendField("ближайший моб:")
+	    .appendField(new Blockly.FieldCheckbox("TRUE"), "zaxvat")
+	    .appendField("или")
+	    .appendField(new Blockly.FieldDropdown([["Человек","PLAYER"], ["Зомби","ZOMBIE"], ["Житель","VILLAGER"], ["Корова","COW"]]), "mob_type");
         this.appendDummyInput()
             .appendField("имя:");
-		    this.appendValueInput("mob_name");
+	this.appendValueInput("mob_name");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -639,7 +639,7 @@ Blockly.Blocks['minecraft_momentmove'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Движение на шаг, напр.")
-			      .appendField(new Blockly.FieldDropdown([["x+1", "x+1"], ["x-1", "x-1"], ["z+1", "z+1"], ["z-1", "z-1"]]), "dir");
+	    .appendField(new Blockly.FieldDropdown([["x+1", "x+1"], ["x-1", "x-1"], ["z+1", "z+1"], ["z-1", "z-1"]]), "dir");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -653,7 +653,7 @@ Blockly.Blocks['minecraft_sputnikCraft'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Скрафтить предмет: ")
-			      .appendField(new Blockly.FieldVariable('craft_block'), "item");
+	    .appendField(new Blockly.FieldVariable('craft_block'), "item");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -680,7 +680,7 @@ Blockly.Blocks['minecraft_sputnikInvSlot'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Положить в руку слот в инвентаре: ");
-		    this.appendValueInput("slot").setCheck("Number");;
+	this.appendValueInput("slot").setCheck("Number");;
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -694,8 +694,8 @@ Blockly.Blocks['minecraft_sputnikBuild'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Строить блок из руки бота")
-			      .appendField("напр.")
-			      .appendField(new Blockly.FieldDropdown([["восток", "EAST"], ["север", "NORTH"], ["юг", "SOUTH"], ["запад", "WEST"]]), "dir");
+	    .appendField("напр.")
+	    .appendField(new Blockly.FieldDropdown([["восток", "EAST"], ["север", "NORTH"], ["юг", "SOUTH"], ["запад", "WEST"]]), "dir");
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -724,10 +724,10 @@ Blockly.Blocks['minecraft_sputnikProg'] = {
             .appendField("Запрограммировать автономно.");
         this.appendDummyInput()
             .appendField("Скин по имени:");
-		    this.appendValueInput("mob_name");
-		    this.setInputsInline(true);
-		    this.appendStatementInput("otherCode")
-			      .setCheck(null);        
+	this.appendValueInput("mob_name");
+	this.setInputsInline(true);
+	this.appendStatementInput("otherCode")
+            .setCheck(null);        
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour("#ff9494");
@@ -740,9 +740,9 @@ Blockly.Blocks['minecraft_progSputnikMove'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("[prog] Двигать БОТ за дроном")
-			      .appendField(new Blockly.FieldCheckbox("TRUE"), "drone")
-			      .appendField("или на");
-		    this.appendDummyInput().appendField("x:");
+	    .appendField(new Blockly.FieldCheckbox("TRUE"), "drone")
+	    .appendField("или на");
+	this.appendDummyInput().appendField("x:");
         this.appendValueInput("x") .setCheck("Number");
         this.appendDummyInput().appendField("y:");
         this.appendValueInput("y") .setCheck("Number");
@@ -761,10 +761,10 @@ Blockly.Blocks['minecraft_progSputnikAtack'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("[prog] Атаковать моба");
-		    this.appendValueInput("mob_type");
-		    this.appendDummyInput()
+	this.appendValueInput("mob_type");
+	this.appendDummyInput()
             .appendField("в радиусе");
-		    this.appendValueInput("radius").setCheck("Number");;
+	this.appendValueInput("radius").setCheck("Number");;
         this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
